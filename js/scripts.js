5566c540f79cf14dcc51c6984e2fdc2274759082
Dylan Lewis and Nathan Stewart <dylan@>
2017-05-31 13:18:10 -0700

    add clickable remove function to picture that appears when text is clicked

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
   $("span#display").click(function() {
-    $("div#picture").append("<img src=\"img/fruit.jpg\">");
-
-
+    $("div#picture").prepend("<img src=\"img/fruit.jpg\">");
+    $("div#picture").children().click(function() {
+      $(this).remove();
+    });
   });
 });
